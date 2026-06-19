3cd3a695b43100b794f08d4d9880c41ae39935d6
CyberBill Africa <143273921+cyberbillafrica@users.noreply.github.com>
2026-06-19 10:47:11 +0100

    Create supabase-public.js

---
+++ js/supabase-public.js
@@ -0,0 +1,5 @@
+// Initialize the public Supabase client using your global configuration keys
+const SUPABASE_URL = 'https://afhqcocriroqigcyuqvm.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaHFjb2NyaXJvcWlnY3l1cXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MTQ2MTIsImV4cCI6MjA5NzA5MDYxMn0.3Zd0F2THEDQX0hVX9sA2EoDvoJGv0-B5bkoBfkaXVU0';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
